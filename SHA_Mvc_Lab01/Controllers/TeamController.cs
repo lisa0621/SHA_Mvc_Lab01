403bf7e61a9178ea4e20ecad953e70f3eae776b5
SHA-PAI LI <lisa0621@gmail.com>
2016-06-28 17:01:18 +0800

    Upload Excel
    相關的工作項目: #13

--- SHA_Mvc_Lab01/Controllers/TeamController.cs
+++ SHA_Mvc_Lab01/Controllers/TeamController.cs
@@ -1,5 +1,11 @@
 ﻿using MotoGP.Service.Interface;
 using MotoGP.ViewModels.Team;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace SHA_Mvc_Lab01.Controllers
@@ -97,6 +103,105 @@ namespace SHA_Mvc_Lab01.Controllers
             {
                 return View();
             }
+        }
+
+        private string fileSavedPath = WebConfigurationManager.AppSettings["UploadPath"];
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            JObject jo = new JObject();
+            string result = string.Empty;
+
+            if (file == null)
+            {
+                jo.Add("Result", false);
+                jo.Add("Msg", "請上傳檔案!");
+                result = JsonConvert.SerializeObject(jo);
+                return Content(result, "application/json");
+            }
+            if (file.ContentLength <= 0)
+            {
+                jo.Add("Result", false);
+                jo.Add("Msg", "請上傳正確的檔案.");
+                result = JsonConvert.SerializeObject(jo);
+                return Content(result, "application/json");
+            }
+
+            string fileExtName = Path.GetExtension(file.FileName).ToLower();
+
+            if (!fileExtName.Equals(".xls", StringComparison.OrdinalIgnoreCase)
+                &&
+                !fileExtName.Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+            {
+                jo.Add("Result", false);
+                jo.Add("Msg", "請上傳 .xls 或 .xlsx 格式的檔案");
+                result = JsonConvert.SerializeObject(jo);
+                return Content(result, "application/json");
+            }
+
+            try
+            {
+                var uploadResult = this.FileUploadHandler(file);
+
+                jo.Add("Result", !string.IsNullOrWhiteSpace(uploadResult));
+                jo.Add("Msg", !string.IsNullOrWhiteSpace(uploadResult) ? uploadResult : string.Empty);
+
+                result = JsonConvert.SerializeObject(jo);
+            }
+            catch (Exception ex)
+            {
+                jo.Add("Result", false);
+                jo.Add("Msg", ex.Message);
+                result = JsonConvert.SerializeObject(jo);
+            }
+            return Content(result, "application/json");
+        }
+
+        /// <summary>
+        /// Files the upload handler.
+        /// </summary>
+        /// <param name="file">The file.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">file;上傳失敗：沒有檔案！</exception>
+        /// <exception cref="System.InvalidOperationException">上傳失敗：檔案沒有內容！</exception>
+        private string FileUploadHandler(HttpPostedFileBase file)
+        {
+            string result;
+
+            if (file == null)
+            {
+                throw new ArgumentNullException("file", "上傳失敗：沒有檔案！");
+            }
+            if (file.ContentLength <= 0)
+            {
+                throw new InvalidOperationException("上傳失敗：檔案沒有內容！");
+            }
+
+            try
+            {
+                string virtualBaseFilePath = Url.Content(fileSavedPath);
+                string filePath = HttpContext.Server.MapPath(virtualBaseFilePath);
+
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+
+                string newFileName = string.Concat(
+                    DateTime.Now.ToString("yyyyMMddHHmmssfff"),
+                    Path.GetExtension(file.FileName).ToLower());
+
+                string fullFilePath = Path.Combine(Server.MapPath(fileSavedPath), newFileName);
+                file.SaveAs(fullFilePath);
+
+                result = newFileName;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return result;
         }
     }
 }

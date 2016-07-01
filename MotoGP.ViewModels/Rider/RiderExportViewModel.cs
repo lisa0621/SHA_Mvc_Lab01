36e3f3dd0d156cc1e3429e5a4cbba1c6f1ab9aad
SHA-PAI LI <lisa0621@gmail.com>
2016-07-01 14:11:56 +0800

    一條龍匯出自訂資料欄位
    相關的工作項目: #14

---
+++ MotoGP.ViewModels/Rider/RiderExportViewModel.cs
@@ -0,0 +1,45 @@
+﻿using MotoGP.Models.Enum;
+using SHA_Mvc_Lab01.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoGP.ViewModels.Rider
+{
+    public class RiderExportViewModel
+    {
+        public int Id { get; set; }
+
+        [ExportColumn(Name = "Name", Order = 1)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [ExportColumn(Name = "Birth", Order = 3)]
+        public DateTime Birth { get; set; }
+
+        [ExportColumn(Name = "Country", Order = 4)]
+        public string Country { get; set; }
+
+        [ExportColumn(Name = "City", Order = 5)]
+        public string City { get; set; }
+
+        [ExportColumn(Name = "Sex", Order = 6)]
+        public string Sex { get; set; }
+
+        [ExportColumn(Name = "Weight", Order = 7)]
+        public double Weight { get; set; }
+
+        [ExportColumn(Name = "Height", Order = 8)]
+        public double Height { get; set; }
+
+        [ExportColumn(Name = "Introduction", Order = 9)]
+        public string Introduction { get; set; }
+
+        [ExportColumn(Name = "Classes", Order = 2)]
+        public EnumFlagGameClasses Classes { get; set; }
+    }
+}
--- SHA_Mvc_Lab01/Controllers/RiderController.cs
+++ SHA_Mvc_Lab01/Controllers/RiderController.cs
@@ -1,5 +1,12 @@
-﻿using MotoGP.Service.Interface;
+﻿using AutoMapper;
+using MotoGP.Service.Interface;
 using MotoGP.ViewModels.Rider;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SHA_Mvc_Lab01.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace SHA_Mvc_Lab01.Controllers
@@ -17,7 +24,20 @@ namespace SHA_Mvc_Lab01.Controllers
         // GET: Rider
         public ActionResult Index()
         {
+            //匯出資料欄位
+            var exportColumns = ExportColumnAttributeHelper<RiderExportViewModel>.GetExportColumns()
+                    .Select(c => new SelectListItem()
+                    {
+                        Value = c.ColumnName,
+                        Text = c.Name,
+                        Selected = true
+                    })
+                    .ToList();
+
+            ViewBag.ExportColumns = exportColumns;
+            
             var data = riderService.GetList();
+
             return View(data);
         }
 
@@ -95,6 +115,44 @@ namespace SHA_Mvc_Lab01.Controllers
             {
                 return View();
             }
+        }
+
+        [HttpPost]
+        public ActionResult HasData()
+        {
+            JObject jo = new JObject();
+            bool result = !riderService.GetList().Count.Equals(0);
+            jo.Add("Msg", result.ToString());
+            return Content(JsonConvert.SerializeObject(jo), "application/json");
+        }
+
+        public ActionResult Export(string fileName, string selectedColumns)
+        {
+            //要匯出的資料
+            var data = riderService.GetList();
+
+            var config = new MapperConfiguration(cfg =>
+                cfg.CreateMap<RiderItemViewModel, RiderExportViewModel>()
+                   .ForMember(d => d.Sex, o => o.MapFrom(s => s.Sex ? "Male" : "Female")));
+
+            var mapper = config.CreateMapper();
+            var result = mapper.Map<List<RiderExportViewModel>>(data);
+
+            var dt = ExportDataHelper.GetExportDataTable(result, selectedColumns);
+
+            var exportFileName = string.IsNullOrWhiteSpace(fileName)
+                ? string.Concat(
+                    "RiderData_",
+                    DateTime.Now.ToString("yyyyMMddHHmmss"),
+                    ".xlsx")
+                : string.Concat(fileName, ".xlsx");
+
+            return new ExportExcelResult
+            {
+                SheetName = "Rider",
+                FileName = exportFileName,
+                ExportData = dt
+            };
         }
     }
 }
